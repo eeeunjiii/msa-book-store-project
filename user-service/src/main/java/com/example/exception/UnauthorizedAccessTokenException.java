701429ea6373dd57ce0b4d619110f849ae521c9d
eeeunjiii <jully1010@naver.com>
2026-04-08 21:34:44 +0900

    fix: JWT 관련 예외 삭제

--- user-service/src/main/java/com/example/exception/UnauthorizedAccessTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.example.exception;
-
-import com.example.constant.ErrorCode;
-
-public class UnauthorizedAccessTokenException extends CustomException {
-    public UnauthorizedAccessTokenException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-}
